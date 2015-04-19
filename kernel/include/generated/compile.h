b6c20d674fea7b770c5f28b1eb808dfc75a2e63e
M <m@i5.(none)>
2015-04-19 07:51:28 -0500

    add kernel tree

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 9 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#9 PREEMPT Fri Apr 11 19:02:33 CDT 2014"
+#define LINUX_COMPILE_TIME "19:02:33"
+#define LINUX_COMPILE_BY "m"
+#define LINUX_COMPILE_HOST "i5"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
